5d8bb975a85ffa75b3464c10c34033dbabc0ab94
lenovo-p17169510 <lenovo-p17169510@users.noreply.github.com>
2021-02-13 23:30:00 +0300

    Добавлены функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,12 @@
 package ru.stqa.pft.sandbox;
 
 class MyFirstProgram {
-public static void main(String[]args){
-    System.out.println("Hello,world");
-        }
-        }
+  public static void main(String[] args) {
+    hello("world");
+
+  }
+
+  public static void hello(String name) {
+    System.out.printf("Hello,%s \n", name);
+  }
+}
